ea2e389e7ea2531304c05ea8230b90339b7076d0
Sajjad Hashemian <wolaws@gmail.com>
2016-08-18 01:10:40 +0430

    Init

---
+++ README.md
@@ -0,0 +1,31 @@
+[slack-link]: https://fisherman-wharf.herokuapp.com
+[slack-badge]: https://fisherman-wharf.herokuapp.com/badge.svg
+[fisherman]: https://github.com/fisherman/fisherman
+
+[![Slack Room][slack-badge]][slack-link]
+
+# Transfer
+
+Upload to <https://transfer.sh>
+
+## Install
+
+With [fisherman]
+
+```
+fisher transfer
+```
+
+## Usage
+
+```fish
+transfer my-file.txt
+```
+
+```fish
+transfer my-file.txt my-file-new-name.txt
+```
+
+```fish
+echo my message text | transfer my-message.txt
+```
---
+++ transfer.fish
@@ -0,0 +1,35 @@
+function transfer -d "Upload to transfer.sh" -a file name
+  set -l tmp (mktemp -t transferXXX)
+
+  if test -z $name
+    if not isatty
+      set name $file
+    else if test -r $file
+      set name (basename $file)
+    end
+  end
+
+  if test -z $name
+    set name (random)
+  end
+
+  if not isatty
+    set file ""
+  end
+
+  set name (echo $name | sed -e 's/[^a-zA-Z0-9._-]/-/g')
+  set name (echo $name | sed -e 's/-\{1,\}/-/g')
+
+  if test -n $file
+    if not test -r $file
+      echo "transfer: can not read the file." > /dev/stderr
+      return 1
+    end
+    curl --progress-bar --upload-file $file https://transfer.sh/$name >> $tmp
+  else
+    curl --progress-bar --upload-file - https://transfer.sh/$name >> $tmp
+  end
+
+  cat $tmp
+  rm -f $tmp
+end
